b32a7e8d9ad542640d1dfd41bda2b97bcd6fdfff
TUAN NGUYEN <118437906+JONNYDAN@users.noreply.github.com>
2024-08-03 17:37:37 +0700

    Update edit-game.php

--- edit-game.php
+++ edit-game.php
@@ -13,7 +13,7 @@ include ('includes/header.php');
 </head>
 
 <?php
-if (isset($_SESSION['user']) && isset($_GET['id']) $_SESSION['user']['id_user']) {
+if (isset($_SESSION['user']) && isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
     $stmt = $pdo->prepare("SELECT * FROM games WHERE id_game = ? AND id_user");
